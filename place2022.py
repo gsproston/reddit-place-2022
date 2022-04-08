3c068a20ea736e53b6eee004ad708ae124c27e0e
gsproston <gsproston94@gmail.com>
2022-04-08 23:42:31 +0100

    Script to save final image

--- place2022.py
+++ place2022.py
@@ -1,19 +1,22 @@
+from PIL import Image
+import numpy as np
+
 # open file
-import csv
-
-
 file = open('2022_place_canvas_history.csv', 'r')
 
 # read header line
 line = file.readline()
 line = file.readline()
 lineCount = 1
-width = 0
-height = 0
+
+# width and height of the canvas
+dims = 4000
+# init the canvas as white
+finalCanvas = [ [(0xFF,0xFF,0xFF)] * dims for i in range(dims)]
 
 while len(line) > 0:
     # print rudimentary loading bar
-    if lineCount % 10000 == 0:
+    if lineCount % 100000 == 0:
         print('.', end='')
         lineCount = 0
 
@@ -26,29 +29,23 @@ while len(line) > 0:
 
     date = fields[0]
     userIdHash = fields[1]
-    colour = fields[2]
+    # get colour information
+    r = int(fields[2][1:3], 16)
+    g = int(fields[2][3:5], 16)
+    b = int(fields[2][5:],  16)
     # trim the leading '"'
-    x = fields[3][1:]
-    # trim the ending '"' and '\n'
-    y = fields[4][:-2]
+    x = int(fields[3][1:])
+    # trim the trailing '"' and '\n'
+    y = int(fields[4][:-2])
 
-    # check x and y are ints 
-    if not x.isdecimal() or not y.isdecimal():
-        print("Could not parse co-ordinates")
-        break
-
-    # convert to ints
-    x = int(x)
-    y = int(y)
-
-    # calculate the width and height of the canvas
-    if x > width:
-        width = x
-    if y > height:
-        height = y
+    finalCanvas[x][y] = (r, g, b)
 
     # read the next line
     line = file.readline()
     lineCount += 1
 
-print("Width: "  + x + " Height: " + y)
+# Convert the pixels into an array using numpy
+array = np.array(finalCanvas, dtype=np.uint8)
+# Use PIL to create an image from the new array of pixels
+finalImage = Image.fromarray(array)
+finalImage.save('finalCanvas.png')
