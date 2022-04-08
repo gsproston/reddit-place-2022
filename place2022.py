df8d8fdcf57abeb317c0e2638c007dabc3cdca2f
gsproston <gsproston94@gmail.com>
2022-04-08 22:15:39 +0100

    Initial commit

---
+++ place2022.py
@@ -0,0 +1,34 @@
+# open file
+import csv
+
+
+file = open('2022_place_canvas_history.csv', 'r')
+
+# read header line
+line = file.readline()
+line = file.readline()
+
+while len(line) > 0:
+    print(line)
+    # parse out each field
+    fields = line.split(',')
+    # make sure there are enough fields
+    if len(fields) < 5:
+        print("Too few fields")
+        break
+
+    date = fields[0]
+    userIdHash = fields[1]
+    colour = fields[2]
+    # trim the leading '"'
+    x = fields[3][1:]
+    # trim the ending '"' and '\n'
+    y = fields[4][:-2]
+
+    # check x and y are ints 
+    if not x.isdecimal() or not y.isdecimal():
+        print("Could not parse co-ordinates")
+        break
+
+    # read the next line
+    line = file.readline()
