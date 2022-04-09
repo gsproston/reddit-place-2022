7cea1263bb8488d9c7cc2422ee31db89340404ac
gsproston <gsproston94@gmail.com>
2022-04-09 13:15:42 +0100

    Method for getting PixelInfo object from line

--- place2022.py
+++ place2022.py
@@ -5,10 +5,36 @@ from PIL import Image
 import numpy as np
 
 class PixelInfo:
-    def __init__(self, date, userIdHash, colour):
+    def __init__(self, date, userIdHash, colour, coords):
         self.date = date
         self.userIdHash = userIdHash
         self.colour = colour
+        self.coords = coords
+
+def getPixelInfo(infoStr):
+    # parse out each field
+    fields = infoStr.split(',')
+    # make sure there are enough fields
+    if len(fields) < 5:
+        print("Too few fields")
+        return NULL
+
+    try:
+        date = datetime.strptime(fields[0], "%Y-%m-%d %H:%M:%S.%f %Z")
+    except ValueError:
+        date = datetime.strptime(fields[0], "%Y-%m-%d %H:%M:%S %Z")
+
+    userIdHash = fields[1]
+    # get colour information
+    r = int(fields[2][1:3], 16)
+    g = int(fields[2][3:5], 16)
+    b = int(fields[2][5:],  16)
+    # trim the leading '"'
+    x = int(fields[3][1:])
+    # trim the trailing '"' and '\n'
+    y = int(fields[4][:-2])
+
+    return PixelInfo(date, userIdHash, (r, g, b), (x, y))
 
 # open file
 fileName = os.path.join('input', '2022_place_canvas_history.csv')
@@ -31,32 +57,14 @@ while len(line) > 0:
         print('.', end='')
         lineCount = 0
 
-    # parse out each field
-    fields = line.split(',')
-    # make sure there are enough fields
-    if len(fields) < 5:
-        print("Too few fields")
-        break
-
-    try:
-        date = datetime.strptime(fields[0], "%Y-%m-%d %H:%M:%S.%f %Z")
-    except ValueError:
-        date = datetime.strptime(fields[0], "%Y-%m-%d %H:%M:%S %Z")
-
-    userIdHash = fields[1]
-    # get colour information
-    r = int(fields[2][1:3], 16)
-    g = int(fields[2][3:5], 16)
-    b = int(fields[2][5:],  16)
-    # trim the leading '"'
-    x = int(fields[3][1:])
-    # trim the trailing '"' and '\n'
-    y = int(fields[4][:-2])
-
-    # only hold the pixel info if there's no entry or the entry is later
-    if (finalCanvasInfo[x][y] == NULL or 
-        finalCanvasInfo[x][y].date < date):
-        finalCanvasInfo[x][y] = PixelInfo(date, userIdHash, (r, g, b))
+    pixelInfo = getPixelInfo(line)
+    if (pixelInfo != NULL):
+        x = pixelInfo.coords[0]
+        y = pixelInfo.coords[1]
+        # only hold the pixel info if there's no entry or the entry is later
+        if (finalCanvasInfo[x][y] == NULL or 
+            finalCanvasInfo[x][y].date < pixelInfo.date):
+            finalCanvasInfo[x][y] = pixelInfo
 
     # read the next line
     line = file.readline()
